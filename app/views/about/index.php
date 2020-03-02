489aa5c0e514d2e12efdca1ad15084ac27736a63
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-03-03 06:57:02 +0700

    melakukan sedikit perubahan

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -5,9 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Index</title>
+    <style>
+        span {
+            color: red;
+
+        }
+    </style>
 </head>
 <h1>Ini adalah halaman about Index</h1>
-<p>Hai , Nama saya <?= $data['nama'] ?> , saya seorang <?= $data['pekerjaan'] ?> dan berumur <?= $data['umur'] ?> tahun</p>
+<p>Hai , Nama saya <span><?= $data['nama'] ?> </span>, saya seorang <span><?= $data['pekerjaan'] ?></span> dan berumur <span><?= $data['umur'] ?></span> tahun</p>
 
 <body>
 
