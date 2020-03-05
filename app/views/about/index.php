0440e05fe6115e951e25ff46be741c0a5c0adda8
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-03-05 22:56:25 +0700

    add images

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>Ini adalah halaman about Index</h1>
-<p>Hai , Nama saya <span><?= $data['nama'] ?> </span>, saya seorang <span><?= $data['pekerjaan'] ?></span> dan berumur <span><?= $data['umur'] ?></span> tahun</p>
+<div class="container ml-5 mt-3">
+    <h1>About Me</h1>
+    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.liputan6.com%2Fshowbiz%2Fread%2F3654320%2Ffokus-bisnis-ricky-harun-berhenti-jadi-artis&psig=AOvVaw16xPj1DtT4YBbwJdvnpQbp&ust=1583510686494000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJjgpejYg-gCFQAAAAAdAAAAABAD" alt="ricky" class="img-thumbnail">
+    <p>Hai , Nama saya <span><?= $data['nama'] ?> </span>, saya seorang <span><?= $data['pekerjaan'] ?></span> dan berumur <span><?= $data['umur'] ?></span> tahun</p>
+</div>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -10,7 +10,8 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="http://localhost/mvc/public">PENTA</a>
+        <a class="navbar-brand" href="http://localhost/mvc/public">PENTA
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
