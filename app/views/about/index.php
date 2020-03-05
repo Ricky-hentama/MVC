cc3424cffc99469804276ec4205073354738e927
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-03-05 21:58:35 +0700

    .

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,16 +1,2 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Features</a>
-            <a class="nav-item nav-link" href="#">Pricing</a>
-            <a class="nav-item nav-link disabled" href="#">Disabled</a>
-        </div>
-    </div>
-</nav>
 <h1>Ini adalah halaman about Index</h1>
 <p>Hai , Nama saya <span><?= $data['nama'] ?> </span>, saya seorang <span><?= $data['pekerjaan'] ?></span> dan berumur <span><?= $data['umur'] ?></span> tahun</p>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -9,3 +9,17 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="http://localhost/mvc/public">PENTA</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="http://localhost/mvc/public">Home <span class="sr-only">(current)</span>
+                </a>
+                <a class="nav-item nav-link" href="http://localhost/mvc/public/about">About </a>
+                <a class="nav-item nav-link" href="http://localhost/mvc/public/about/page">Page</a>
+            </div>
+        </div>
+    </nav>
