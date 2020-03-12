0935c4458f6d12ab732634b41ad914b7a1403044
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-03-12 22:56:13 +0700

    add template profil for about page

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,5 +1,43 @@
-<div class="container ml-5 mt-3">
-    <h1>About Me</h1>
-    <img src="http://localhost/mvc/public/img/rickyy.png" alt="ricky" class="profil shadow" width="200px">
-    <p>Hai , Nama saya <span><?= $data['nama'] ?> </span>, saya seorang <span><?= $data['pekerjaan'] ?></span> dan berumur <span><?= $data['umur'] ?></span> tahun</p>
+<!-- test template -->
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="container" ">
+    <div class=" row">
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <h4>About Me</h4>
+        </div>
+        <div class="panel-body shadow">
+            <div class=" col-md-4 col-xs-12 col-sm-6 col-lg-4">
+                <img alt="Ricky Pic" src="http://localhost/mvc/public/img/guru.jpg" id="profile-image1" width="300px" class="img-circle img-responsive">
+
+
+            </div>
+            <div class="col-md-8 col-xs-12 col-sm-6 col-lg-8">
+                <div class="container">
+                    <h2 style="display: inline-block">RICKY HENTAMA</h2> <img src="https://i.ya-webdesign.com/images/instagram-verified-badge-png.png" width="37px" alt="vry" style="margin-bottom: 13px;margin-left:10px;">
+                    <p>an <b> Teacher</b></p>
+
+
+
+                </div>
+                <hr>
+                <ul class="container details">
+                    <li>
+                        <p><span class="glyphicon glyphicon-user one" style="width:50px;"></span>belum_tamvan</p>
+                    </li>
+                    <li>
+                        <p><span class="glyphicon glyphicon-envelope one" style="width:50px;"></span>rickyhendrapratama4@gmail.com</p>
+                    </li>
+                </ul>
+                <hr>
+                <div class="col-sm-5 col-xs-6 tital "> Hai , saya Ricky. saya adalah lulusan fakultas Teknik dari Universitas Gadjah Mada dan sekarang saya mengajar di SMK Negeri 1 Purwokerto </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- test template -->
 </div>
