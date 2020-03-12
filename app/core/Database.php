a3537530d024425dbe35cb124c98a50a90bde872
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-03-12 08:01:58 +0700

    uodate Database core

--- app/core/Database.php
+++ app/core/Database.php
@@ -9,4 +9,54 @@ class Database
 
     private $dbh;
     private $statment;
+
+    public function __construct()
+    {
+        //data source name
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name;
+        $option = [
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+
+        ];
+        try {
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $option);
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public function query($query)
+    {
+        $this->statment = $this->dbh->prepare($query);
+    }
+    public function bind($param, $value, $type = null)
+    {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+                    break;
+            }
+        }
+        $this->statment->bindValue($param, $value, $type);
+    }
+    public function execute()
+    {
+        $this->statment->execute();
+    }
+    public function resultSet()
+    {
+        $this->execute();
+        return $this->statment->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
