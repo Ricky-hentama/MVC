ee113249418ec432a999dd952a25ef87f044325e
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-03-15 00:23:46 +0700

    update teacher, insert teacher

--- app/views/teacher/index.php
+++ app/views/teacher/index.php
@@ -1,7 +1,12 @@
 <div class="container">
     <div class="row">
-        <div class="col-6">
-            <h3>Daftar Guru</h3>
+        <div class="col-6 mt-5">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                Launch demo modal
+            </button>
+
+            <h3 class="mt-3 ">Daftar Guru Penta</h3>
 
             <ul class="list-group">
                 <?php foreach ($data['guru'] as $guru) : ?>
@@ -13,3 +18,24 @@
 
         </div>
     </div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus sequi, quasi iure nam maiores cumque adipisci qui laboriosam aspernatur reprehenderit incidunt velit eos dignissimos animi quae aperiam distinctio nostrum iste?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
