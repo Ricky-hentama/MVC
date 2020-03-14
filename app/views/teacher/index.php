3bb8226a8eb884fd86123835f917908543ba1551
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-03-14 07:03:57 +0700

    update teacher view

--- app/views/teacher/index.php
+++ app/views/teacher/index.php
@@ -3,29 +3,18 @@
         <div class="col-6">
             <h3>Daftar Guru</h3>
 
-            <table class="table">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">id</th>
-                        <th scope="col">Nama</th>
-                        <th scope="col">Mapel</th>
-                        <th scope="col">Umur</th>
-                        <th scope="col">Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($data['guru'] as $guru) : ?>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td><?= $guru['nama'] ?></td>
-                            <td><?= $guru['mapel'] ?></td>
-                            <td><?= $guru['umur'] ?></td>
-                            <td><?= $guru['status'] ?></td>
-                        </tr>
-                    <?php endforeach; ?>
+            <ul class="list-group">
+                <?php foreach ($data['guru'] as $guru) : ?>
+                    <li class="list-group-item d-flex justify-content-between align-items-center"><?= $guru["nama"] ?>
+                        <a href="" class="badge badge-primary ">detail</a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
 
-                </tbody>
-            </table>
+
+
+
+
+
         </div>
     </div>
-</div>
