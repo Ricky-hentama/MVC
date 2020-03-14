087fda21756a2080c2d1afcbe33a24fd144a195e
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-03-15 00:35:38 +0700

    update index

--- app/views/teacher/index.php
+++ app/views/teacher/index.php
@@ -2,8 +2,8 @@
     <div class="row">
         <div class="col-6 mt-5">
             <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                Launch demo modal
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+                Tambah Data Guru
             </button>
 
             <h3 class="mt-3 ">Daftar Guru Penta</h3>
@@ -19,22 +19,24 @@
         </div>
     </div>
 </div>
+
+
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <h5 class="modal-title" id="judulModal">Tambah Data</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus sequi, quasi iure nam maiores cumque adipisci qui laboriosam aspernatur reprehenderit incidunt velit eos dignissimos animi quae aperiam distinctio nostrum iste?</p>
+
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Batal</button>
+                <button type="button" class="btn btn-primary">Tambah</button>
             </div>
         </div>
     </div>
