0988202e8ab7192941bcc1b600ca938d335bc7c4
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-03-17 06:36:49 +0700

    create Flasher at core

---
+++ app/core/Flasher.php
@@ -0,0 +1,13 @@
+<?php
+
+class Flasher
+{
+    public static function setFlash($pesan, $aksi, $tipe)
+    {
+        $_SESSION['flash'] = [
+            'pesan' => $pesan,
+            'aksi' => $aksi,
+            'tipe' => $tipe
+        ];
+    }
+}
