81149ab49bd0d48d9185dcbae5de8716b698b513
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-03-19 06:50:56 +0700

    add button update data

--- app/views/teacher/index.php
+++ app/views/teacher/index.php
@@ -23,6 +23,7 @@
                 <?php foreach ($data['guru'] as $guru) : ?>
                     <li class="list-group-item "><?= $guru["nama"] ?>
                         <a href="<?= BASEURL; ?>teacher/hapus/<?= $guru['id'] ?>" class="badge badge-danger float-right ml-1" onclick="return confirm('Apakah anda yakin ingin menghapus <?= $guru["nama"] ?> ?');">Hapus</a>
+                        <a href="<?= BASEURL; ?>teacher/ubah/<?= $guru['id'] ?>" class="badge badge-warning float-right ml-1">Ubah</a>
                         <a href="<?= BASEURL; ?>teacher/detail/<?= $guru['id'] ?>" class="badge badge-primary float-right ml-1">Detail</a>
                     </li>
                 <?php endforeach; ?>
