16cf6f4d831cd312fc9d5e7b864660a5041863d2
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-03-22 22:25:37 +0700

    .

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
+  let id = [];
   $("#tambahDataGuru").click(function() {
     $("#judulModal").html("Tambah Data Guru");
     $(".modal-footer button[type=submit]").html("Tambah");
   });
   $("#ubah").click(function() {
     $("#judulModal").html("Ubah Data Guru");
-    $(".modal-footer button[type=submit]").html("Ubh");
-    const id = $(this).data("id");
-    console.log(id);
+    $(".modal-footer button[type=submit]").html("Ubah");
+    console.log(this);
   });
-  alert("ok");
 });
