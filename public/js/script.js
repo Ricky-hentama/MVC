b17173caff462d70be2e80b549d7f1878a5a74e1
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-04-06 22:14:24 +0700

    update script

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,13 @@
-$(document).ready(function() {
-  $("#tambahDataGuru").click(function() {
+$(document).ready(function () {
+  $("#tambahDataGuru").click(function () {
     $("#judulModal").html("Tambah Data Guru");
     $(".modal-footer button[type=submit]").html("Tambah");
   });
-  $("#ubah").click(function() {
+  $("#ubah").click(function (e) {
     $("#judulModal").html("Ubah Data Guru");
     $("#tombol-aksi").html("Ubah");
-    const id = $(this).data("id");
-    console.log(id);
+    // const id = $(this).data("id");
+    alert(ok);
+    console.log(e);
   });
 });
